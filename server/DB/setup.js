035f9b9f4104adc5bad3c2509cbce977b9471808
Naeem Shah <naeemultimate@gmail.com>
2025-02-25 15:14:09 +0000

    add setup connect

--- server/DB/setup.js
+++ server/DB/setup.js
@@ -1,11 +1,23 @@
-require('dotenv').config();
-const fs = require('fs');
-const db = require('./connect');
-// const sql = fs.readFileSync('../db/quiz.sql').toString();
+require("dotenv").config();
+const { Connection } = require("databricks-sql");
 
-db.query(sql)
-    .then(data => {
-        db.end();
-        console.log("Set-up complete.")
-    })
-    .catch(error => console.log(error));
+const dbConfig = {
+  server: process.env.DATABRICKS_HOST,
+  token: process.env.DATABRICKS_ACCESS_TOKEN,
+  httpPath: process.env.DATABRICKS_HTTP_PATH,
+  port: process.env.DATABRICKS_PORT || 443,
+};
+
+// Function to connect to Databricks
+async function getDatabricksConnection() {
+  const conn = new Connection(dbConfig);
+  try {
+    await conn.connect();
+    console.log("✅ Connected to Databricks");
+    return conn;
+  } catch (error) {
+    console.error("❌ Databricks Connection Error:", error);
+  }
+}
+
+module.exports = { getDatabricksConnection };
